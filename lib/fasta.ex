ce3a2b1bf84b3551dfc66b41a63853b3ec6d5078
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-11 18:16:11 -0400

    Make some FASTA functions private

--- lib/fasta.ex
+++ lib/fasta.ex
@@ -4,7 +4,7 @@ defmodule FASTA do
     |> Enum.map(&parse_datum/1)
   end
 
-  def parse_datum(datum_string) do
+  defp parse_datum(datum_string) do
     [header_string, sequence_string] = Regex.split(~r/\n/, datum_string, parts: 2)
 
     header_str = clean_header(header_string)
@@ -13,17 +13,17 @@ defmodule FASTA do
     %FASTA.Datum{header: header_str, sequence: sequence_str}
   end
 
-  def clean_header(raw_header) do
+  defp clean_header(raw_header) do
     raw_header
     |> String.lstrip(?>)
     |> String.strip
   end
 
-  def clean_sequence(raw_sequence) do
+  defp clean_sequence(raw_sequence) do
     Regex.replace(~r/\s+/, raw_sequence, "")
   end
 
-  def fasta_separator_regex do
+  defp fasta_separator_regex do
     ~r/\s+>/
   end
 end
