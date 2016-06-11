4dc0ba10d3c891bd1d9d39b1a06fb6edc16aab7c
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-11 17:45:18 -0400

    Update FASTA parser

--- lib/fasta.ex
+++ lib/fasta.ex
@@ -1,7 +1,6 @@
 defmodule FASTA do
-  def get_sequence(fasta_datum) do
-    fasta_datum.sequence
-  end
+  def get_sequence(%{sequence: seq}), do: seq
+  def get_sequence(_), do: nil
 
   def parse(fasta_string) do
     Regex.split(fasta_separator_regex, fasta_string)
@@ -9,26 +8,28 @@ defmodule FASTA do
   end
 
   def parse_datum(datum_string) do
-    [metadata_string, sequence_string] = Regex.split(~r/\n/, datum_string, parts: 2)
-    metadata = parse_metadata_string(metadata_string)
-    sequence_data = parse_sequence_string(sequence_string)
-    Map.merge(metadata, sequence_data)
+    [header_string, sequence_string] = Regex.split(~r/\n/, datum_string, parts: 2)
+    header_data = parse_header(header_string)
+    sequence_data = parse_sequence(sequence_string)
+    Map.merge(header_data, sequence_data)
   end
 
-  def parse_metadata_string(metadata_string) do
-    %{metadata: metadata_string} # TODO:
+  def parse_header(raw_header) do
+    %{header: clean_header(raw_header)}
   end
 
-  def parse_sequence_string(raw_sequence) do
+  def parse_sequence(raw_sequence) do
     %{sequence: clean_sequence(raw_sequence)}
   end
 
-  def clean_sequence(raw_sequence) do
-    Regex.replace(~r/[^#{valid_sequence_characters}]/, raw_sequence, "")
+  def clean_header(raw_header) do
+    raw_header
+    |> String.lstrip(?>)
+    |> String.strip
   end
 
-  def valid_sequence_characters do
-    "ACGTURYKMSWBDHVN-"
+  def clean_sequence(raw_sequence) do
+    Regex.replace(~r/\s+/, raw_sequence, "")
   end
 
   def fasta_separator_regex do
