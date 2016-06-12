1ad0b610d9a54138dfd56ca42bbd92e48e4a59af
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-12 11:55:03 -0400

    DRY up FASTA test cases

--- test/fasta_test.exs
+++ test/fasta_test.exs
@@ -19,50 +19,24 @@ defmodule FASTATest do
   end
 
   test "parse extracts data from a valid fasta string" do
-    raw_fasta = ">#{header1}
-                 #{sequence1}
+    raw_valid_fastas = [
+      ">#{header1}\n#{sequence1}\n\n> #{header2}\n#{sequence2}",
+      "\n>#{header1}\n\n#{sequence1}\n\n> #{header2}\n#{sequence2}\n"
+    ]
 
-                 > #{header2}
-                 #{sequence2}"
-    fasta_data = FASTA.parse(raw_fasta)
+    Enum.map(raw_valid_fastas, fn(raw_fasta) ->
+      [first_datum, second_datum] = FASTA.parse(raw_fasta)
 
-    assert(length(fasta_data) == 2)
+      assert(first_datum.header == header1)
+      assert(first_datum.sequence == Regex.replace(~r/\s+/, sequence1, ""))
 
-    [first_datum, second_datum] = fasta_data
-
-    assert(first_datum.header == header1)
-    assert(first_datum.sequence == Regex.replace(~r/\s+/, sequence1, ""))
-
-    assert(second_datum.header == header2)
-    assert(second_datum.sequence == Regex.replace(~r/\s+/, sequence2, ""))
-  end
-
-  test "parse extracts data from a valid fasta string with strange spacing" do
-    raw_fasta = "
-                 >#{header1}
-
-                 #{sequence1}
-
-                 > #{header2}
-                 #{sequence2}
-
-                 "
-    fasta_data = FASTA.parse(raw_fasta)
-
-    assert(length(fasta_data) == 2)
-
-    [first_datum, second_datum] = fasta_data
-
-    assert(first_datum.header == header1)
-    assert(first_datum.sequence == Regex.replace(~r/\s+/, sequence1, ""))
-
-    assert(second_datum.header == header2)
-    assert(second_datum.sequence == Regex.replace(~r/\s+/, sequence2, ""))
+      assert(second_datum.header == header2)
+      assert(second_datum.sequence == Regex.replace(~r/\s+/, sequence2, ""))
+    end)
   end
 
   test "parse returns an empty list of data from an invalid fasta string" do
-    raw_invalid_fasta = "> #{header1}
-                         > #{header2}"
+    raw_invalid_fasta = "> #{header1}\n> #{header2}"
     fasta_data = FASTA.parse(raw_invalid_fasta)
 
     assert(Enum.empty?(fasta_data))
